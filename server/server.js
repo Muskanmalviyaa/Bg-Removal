b8a43decccb67e3aad9855198b8e567d08ee7304
Muskan malviya <muskanmalvi609@gmail.com>
2025-02-16 15:14:39 +0530

    Webhooks

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 // App config
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.use(cors());
 
 // API routes
 app.get('/', (req, res) => res.send('API Working'));
+app.use('/api/user', userRouter)
 
 // Start server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
